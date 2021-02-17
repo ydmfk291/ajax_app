b8315646ab73decaecfb4069863426ee596352c0
ydmfk291 <78467544+ydmfk291@users.noreply.github.com>
2021-02-17 12:12:54 +0900

    AlaxAppの準備

--- db/migrate/20210127052439_create_posts.rb
+++ db/migrate/20210127052439_create_posts.rb
@@ -1,5 +1,9 @@
-class PostsController < ApplicationController
-  def index
-    @post = Post.find(1)  # 1番目のレコードを@postに代入
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.text :content
+      t.boolean :checked
+      t.timestamps
+    end
   end
 end
